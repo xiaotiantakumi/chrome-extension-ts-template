2f23a29deda692240ff2d8df24d79ff08e766c5b
Takumi Oda <xiaotiantakumi@gmail.com>
2021-10-12 00:50:02 +0900

    service worker上ではaxiosがエラー出るので該当部分を削除

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,3 @@
-import moment from 'moment';
-import axios from 'axios';
-
-const now = moment();
 (async () => {
-  const res = await axios.get('https://www.google.com/search?q=test');
-  console.log({
-    from: 'background.ts',
-    now: now.format('YYYY/MM/DD HH:mm:ss'),
-    data: res.data,
-  })
+  console.log("test");
 })();
